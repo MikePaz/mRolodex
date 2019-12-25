761881d42aea6e542994c4fe399195e373446787
MikePaz <mike.pazarlis@gmail.com>
2019-12-25 16:20:41 +0200

    Adding searchbar + filter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component.jsx";
+import { SearchBox } from "./components/search-box/search-box.component.jsx"
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField:''
     };
   }
 
@@ -17,10 +19,20 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({searchField:e.target.value})
+  }
+
   render() {
+    const {monsters,searchField} = this.state; 
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase()
+    .includes(searchField.toLowerCase()))
+   
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+      <h1>M-Rolodex</h1>
+        <SearchBox placeHolder ='search monsters' handleChange={this.handleChange}/>
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
